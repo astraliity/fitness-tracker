c90e89873ad4926a620255fb644194bd9cf3d175
Aleksandr <alex@motynga.ru>
2026-02-19 10:42:45 +0200

    Add CORS support for frontend development
    django-cors-headers позволяет фронтенду (localhost:5173)
    обращаться к API (localhost:8000).

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Third-party
+    'corsheaders',
     'rest_framework',
 
     # Local
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,6 +136,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# CORS — разрешаем фронтенду обращаться к API
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://localhost:3000',
+]
 
 # Django REST Framework
 REST_FRAMEWORK = {
